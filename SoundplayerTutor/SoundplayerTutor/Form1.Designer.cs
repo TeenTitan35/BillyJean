6e97ec6166159fbcfb366f8d9c3e7cfbd562c2c0
TeenTitan35 <67632745+TeenTitan35@users.noreply.github.com>
2020-10-03 19:50:06 +0300

    Add files via upload

---
+++ SoundplayerTutor/SoundplayerTutor/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SoundplayerTutor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 153);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "БИЛЛИ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 218);
+            this.button3.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 153);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "НАСРАЛ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(299, 417);
+            this.button4.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 153);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "КУВШИН";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 18);
+            this.button5.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(256, 256);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "АУ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(13, 314);
+            this.button6.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(256, 256);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "АУАУ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(569, 18);
+            this.button2.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(455, 552);
+            this.button2.TabIndex = 3;
+            this.button2.Text = ".";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 616);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SoundplayerTutor/SoundplayerTutor/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace SoundplayerTutor
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer _soundPlayer;
+        public Form1()
+        {
+            InitializeComponent();
+            SetBut();
+        }
+
+        public void SetBut()
+        {
+            button2.Image = Image.FromFile("C:\\Games\\джинн.jpg");
+            // Align the image and text on the button.
+            button2.ImageAlign = ContentAlignment.MiddleRight;
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            // Give the button a flat appearance.
+            button2.FlatStyle = FlatStyle.Flat;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _soundPlayer = new SoundPlayer("билли.wav");
+            if (button1.Focused)
+            {
+                _soundPlayer.Play();
+                System.Threading.Thread.Sleep(1);
+            }
+            else
+            {
+                _soundPlayer.Stop();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            _soundPlayer = new SoundPlayer("джинн.wav");
+            if (button2.Focused)
+            {
+                _soundPlayer.Play();
+                System.Threading.Thread.Sleep(1);
+            }
+            else
+            {
+                _soundPlayer.Stop();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _soundPlayer = new SoundPlayer("насрал.wav");
+            if (button3.Focused)
+            {
+                _soundPlayer.Play();
+                System.Threading.Thread.Sleep(1);
+            }
+            else
+            {
+                _soundPlayer.Stop();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _soundPlayer = new SoundPlayer("кувшин.wav");
+            if (button4.Focused)
+            {
+                _soundPlayer.Play();
+                
+                System.Threading.Thread.Sleep(1);
+            }
+            else
+            {
+                _soundPlayer.Stop();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            _soundPlayer = new SoundPlayer("ау.wav");
+            if (button5.Focused)
+            {
+                _soundPlayer.Play();
+                System.Threading.Thread.Sleep(1);
+            }
+            else
+            {
+                _soundPlayer.Stop();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            _soundPlayer = new SoundPlayer("ауау.wav");
+            if (button6.Focused)
+            {
+                _soundPlayer.Play();
+                System.Threading.Thread.Sleep(1);
+            }
+            else
+            {
+                _soundPlayer.Stop();
+            }
+        }
+    }
+}
